08e45f98c8f737546b03a7bf422fc84da6c664e7
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2025-09-11 13:11:03 -0300

    fix: Previne race condition que causa LateInitializationError

--- lib/recaptca_config.dart
+++ lib/recaptca_config.dart
@@ -3,6 +3,9 @@
 * @Company: GTEN SOFTWARE PVT.LTD.
 */
 
+
+import 'dart:async';
+
 import 'package:flutter_gcaptcha_v3/constants.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -10,39 +13,49 @@ class RecaptchaHandler {
   RecaptchaHandler._();
 
   static RecaptchaHandler? _instance;
-  late WebViewController controller;
+
+
+  WebViewController? _controller;
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+
   late String _siteKey;
-
-
   String? _captchaToken;
 
   String get siteKey => _siteKey;
-
   String? get captchaToken => _captchaToken;
 
-  /// Returns an instance using the default [Env].
   static RecaptchaHandler get instance => _instance ??= RecaptchaHandler._();
 
-  /// updates the Web view controller
+
   updateController({required WebViewController controller}) {
-    _instance?.controller = controller;
+    _instance?._controller = controller;
+
+    if (!_instance!._controllerCompleter.isCompleted) {
+      _instance!._controllerCompleter.complete(controller);
+    }
 
     controller.runJavaScript(
         '${AppConstants.readyCaptcha}("${_instance?._siteKey}", "submit")');
   }
 
-  void updateToken({required String generatedToken}){
-    _captchaToken=generatedToken;
+  void updateToken({required String generatedToken}) {
+    _captchaToken = generatedToken;
   }
 
   /// setups the data site key
   setupSiteKey({required String dataSiteKey}) =>
       _instance?._siteKey = dataSiteKey;
 
-  /// Executes and call the  recaptcha API
-  static executeV3({String? action}) {
+  /// Executes and call the recaptcha API
+
+  static Future<void> executeV3({String? action}) async {
     final String userAction = action ?? 'submit';
-    _instance?.controller.runJavaScript(
+
+    final WebViewController controller = await _instance!._controllerCompleter.future;
+
+    // 6. Agora é seguro usar o controller para executar o JavaScript.
+    controller.runJavaScript(
         '${AppConstants.executeCaptcha}("${_instance?._siteKey}", "$userAction")');
   }
 }
